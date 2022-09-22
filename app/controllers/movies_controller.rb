3c531ef320a50283feaa2c2db18e2f6f6d61f9c1
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-09-22 13:48:52 +0000

    []

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,7 @@ class MoviesController < ApplicationController
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
   
   def search
+    
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,15 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "user/registrations",
     sessions: 'user/sessions'
   }
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
