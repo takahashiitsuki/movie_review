d7ce11d31ad6a5b205ad8f4d1effeb4900c92fc1
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-10-09 14:58:26 +0000

    [Finish]movie_genre

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -30,6 +30,9 @@ class Admin::ReviewsController < ApplicationController
   end
 
   def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to reviews_path
   end
 
   private
