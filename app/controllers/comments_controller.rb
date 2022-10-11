6e8efb747e9004f8a2ea3de991f7994d0c36c675
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-10-11 03:29:39 +0000

    [Finish]genre

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    @book=Book.find(params[:book_id])
+  end
+  
+  def destroy
+    BookComment.find(params[:id]).destroy
+    @book=Book.find(params[:book_id])
+  end
+  
+  private
+  
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
