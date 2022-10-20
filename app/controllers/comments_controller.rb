1eeeaffb9990e051a53366b1443d054f1f9ab592
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-10-20 06:16:23 +0000

    [Finish]comment_create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    @review = Review.find(params[:id])
+    @review = Review.find(params[:review_id])
     @comment = @review.comments.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
+    redirect_to review_path(params[:review_id])
   end
   
   def destroy
