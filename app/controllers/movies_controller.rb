45ba4dc457a73f6bb8b047d0a05e9d7f40aa8aa4
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-10-26 00:15:44 +0000

    [Finsish] review

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
     @movie = JSON.parse((Tmdb::Movie.detail(params[:id])).to_json)
     uri = URI.parse("https://api.themoviedb.org/3/movie/#{params[:id]}/watch/providers?api_key=95405e6efc8fc27ba9e99c3e27422cf5")
     response = Net::HTTP.get_response(uri)
-    @providers = JSON.parse(response.body)
+    # @providers = JSON.parse(response.body)
   end
   
   private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
    belongs_to :user
+   belongs_to :movie
    has_many :comments,dependent: :destroy
    
    with_options presence: true, on: :publicize do
