d3ff084e13c71369d796c2aa6391fa67f8eded77
EC2 Default User <itakaha71@gmail.com>
2023-01-05 10:10:34 +0000

    [fix]application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  #apiキー
   require 'themoviedb-api'
   Tmdb::Api.key("95405e6efc8fc27ba9e99c3e27422cf5")
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
   
+  #サインアウト時の処理
   def after_sign_out_path_for(resource)
     if resource == :admin
       admin_session_path
