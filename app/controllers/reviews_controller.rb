aa637c5476a61d068de2fa4261c48d4746302e5d
EC2 Default User <itakaha71@gmail.com>
2023-07-23 05:26:54 +0000

    [Fix]review_controll

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,7 @@ class ReviewsController < ApplicationController
     p params
     @review = ReviewForm.new(params[:review_form])
     @review.user_id = current_user.id
-    @tags = params[:review]&.fetch(:tags, '').split(',')
+    @tags = params[:review_form]&.fetch(:tags, '').split(',')
     p params
     p @review
     if params[:post]
